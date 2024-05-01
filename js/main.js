fe16fc7a4db2d78de6e77f446b21c851840704d7
rasmith <robee_98_99@yahoo.com>
2024-04-30 21:04:16 -0400

    progress update

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const calcDisplay = document.querySelector('calc-display');
+const buttons = document.querySelector('div.button');
+
+buttons.addEventListener('click', captureInput)
+
+let firstNumber = '';
+let secondNumber = '';
+
+// const addNum = document.getElementById("+");
+// const subtractNum = document.getElementById("-");
+// const multiplyNum = document.getElementById("*");
+// const divideNum = document.getElementById("/");
+
+// function clear() {
+//     calcDisplay = null;
+//    }
+
+// function render() {
+//     updateDisplay();
+// }
+function captureInput() {
+    // const newFirstNum = addEventListener('click', updateDisplay)
+    // if(evt.target.classList.contains('button')) {
+    //     if(!evt.target.classList.contains('operation-button')) {
+    //         const newFirstNum = evt.target.id[];
+    //     }
+       
+    // }
+    console.log('test 1 two')
+}
+
+function updateDisplay() {  /**show what the user clicked on and it's result */
+   
+}
+
+// function resetDisplay() {  /**reset display if user clicks on Reset */
+//     const resetFrame = document.querySelector('button.reset-button')
+//     resetFrame.addEventListener('click', clear)
+// }
+
+// function operations(){  /**based on the operation selected, do this*/
+//     if()
+// }
